573eadf7855dbf6bbd47b5b93a819a11c525360a
Oleksandr Hordiichuk <alexanderhordiichuk@gmail.com>
2025-03-30 17:14:00 +0200

    cwebpack is chanching to vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+
+export default defineConfig({
+	base: './',
+	server: {
+		port: 3000
+	},
+
+	plugins: [
+		ViteImageOptimizer({
+			png: {
+				quality: 80,
+			},
+			jpeg: {
+				quality: 80,
+			},
+			jpg: {
+				quality: 80,
+			},
+		}),
+	],
+
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		sourcemap: false,
+		minify: true,
+	}
+})
